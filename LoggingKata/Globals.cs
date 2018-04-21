3c80c6fca4f9831b8ae21d9de567e1992eaf8458
reid <reidbras@gmail.com>
2018-04-21 17:28:56 -0500

    Adding constants to Globals.cs

--- LoggingKata/Globals.cs
+++ LoggingKata/Globals.cs
@@ -4,6 +4,10 @@ namespace LoggingKata
     {
         public const string longitude = "longitude";
         public const string latitude = "latitude";
+        public const double maxLongitude = 180.00;
+        public const double minLongitude = -180.00;
+        public const double maxLatitude = 90.00;
+        public const double minLatitude = -90.00;
         public const string logMessageBeginParsing = "Begin Parsing";
         public const string logMessageInitialized = "Log initialized";
         public const string logMessageReadingFileFrom = "Reading csv file ";
@@ -11,5 +15,7 @@ namespace LoggingKata
         public const string logMessageOneLine = "Warning only one line read from file ";
         public const string logMessageUnableToParseLine = "Unable to parse line - ";
         public const string logMessageLength = "length < 3 Line=";
+        public const string argumentExceptionLongitude = "Invalid Value for longitude greater than 180.00 or less than -180.00";
+        public const string argumentExceptionLatitude = "Invalid Value for latitude greater than 90.00 or less than -90.00";
     }
 }
