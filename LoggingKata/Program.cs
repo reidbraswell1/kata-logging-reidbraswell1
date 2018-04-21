88837ada380c0ed7a79942831dd4cdc9ff8f45ef
reid <reidbras@gmail.com>
2018-04-21 12:30:10 -0500

    Adding initial work to main method of Program.cs

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -9,12 +9,33 @@ namespace LoggingKata
         //Why do you think we use ILog?
         static readonly ILog logger = new TacoLogger();
         const string csvPath = "TacoBell-US-AL.csv";
+        static readonly string path = Environment.CurrentDirectory + csvPath;
 
         static void Main(string[] args)
         {
-            logger.LogInfo("Log initialized");
+            logger.LogInfo(Globals.logMessageInitialized);
+            logger.LogInfo(Globals.logMessageReadingFileFrom + path);
 
-            var lines = File.ReadAllLines(csvPath);
+            var lines = File.ReadAllLines(Environment.CurrentDirectory + csvPath);
+
+            // Log Error if no lines read
+            if (lines.Length == 0)
+            {
+                logger.LogError(Globals.logMessageNoLines + path);
+                return;
+            }
+            // Log Warning if only 1 line
+            if (lines.Length == 1)
+            {
+                logger.LogWarning(Globals.logMessageOneLine + path);
+            }
+
+            var tacoParser = new TacoParser();
+            
+            foreach (var line in lines)
+            {
+
+            }
 
             logger.LogInfo($"Lines: {lines[0]}");
 
