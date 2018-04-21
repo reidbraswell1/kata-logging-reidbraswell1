4f86f687356381fd18792302b6994284eb00eb2b
reid <reidbras@gmail.com>
2018-04-21 12:26:10 -0500

    Adding more messages to Globals class

--- LoggingKata/Globals.cs
+++ LoggingKata/Globals.cs
@@ -4,5 +4,9 @@ namespace LoggingKata
     {
         public const string longitude = "longitude";
         public const string latitude = "latitude";
+        public const string logMessageInitialized = "Log initialized";
+        public const string logMessageReadingFileFrom = "Reading csv file ";
+        public const string logMessageNoLines = "Error no lines read from file ";
+        public const string logMessageOneLine = "Warning only one line read from file ";
     }
 }
