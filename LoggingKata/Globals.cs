2eaf42eba76a816dc4d941e44b7aca6241098e90
reid <reidbras@gmail.com>
2018-04-21 21:33:41 -0500

    Corrected messages

--- LoggingKata/Globals.cs
+++ LoggingKata/Globals.cs
@@ -8,13 +8,14 @@ namespace LoggingKata
         public const double minLongitude = -180.00;
         public const double maxLatitude = 90.00;
         public const double minLatitude = -90.00;
-        public const string logMessageBeginParsing = "Begin Parsing";
+        public const string logMessageBeginParsing = "Begin Parsing ";
         public const string logMessageInitialized = "Log initialized";
         public const string logMessageReadingFileFrom = "Reading csv file ";
         public const string logMessageNoLines = "Error no lines read from file ";
         public const string logMessageOneLine = "Warning only one line read from file ";
         public const string logMessageUnableToParseLine = "Unable to parse line - ";
         public const string logMessageLength = "length < 3 Line=";
+        public const char splitChar = ',';
         public const string argumentExceptionLongitude = "Invalid Value for longitude greater than 180.00 or less than -180.00";
         public const string argumentExceptionLatitude = "Invalid Value for latitude greater than 90.00 or less than -90.00";
     }
