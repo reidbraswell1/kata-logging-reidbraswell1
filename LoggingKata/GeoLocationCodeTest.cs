3ac3ff5e447ee439c51d96be5e9049a3032ff31b
reid <reidbras@gmail.com>
2018-04-22 18:26:41 -0500

    Refactor

--- LoggingKata/GeoLocationCodeTest.cs
+++ LoggingKata/GeoLocationCodeTest.cs
@@ -9,22 +9,33 @@ namespace LoggingKata
         public static double geoLocation()
         {
             // GEOLOCATION CODE SAMPLE
-            Coordinate point1A = new Coordinate();
-            Coordinate point1B = new Coordinate();
-            point1A.Latitude = 32.00;
-            point1A.Longitude = -85.00;
-            point1B.Latitude = 32.50;
-            point1B.Longitude = -85.50;
+            var point1A = new Coordinate()
+            {
+                Latitude = 32.00,
+                Longitude = -85.00
+            };
+            var point1B = new Coordinate()
+            {
+                Latitude = 32.50,
+                Longitude = -85.50
+            };
             return GeoCalculator.GetDistance(point1A, point1B, 1);
         }
         public static double geoCoordinatePortable()
         {
             // GEOLOCATION PORTABLE CODE SAMPLE
-            GeoCoordinate point2A = new GeoCoordinate(32.00, -85.00);
-            GeoCoordinate point2B = new GeoCoordinate(32.50, -85.50);
+            GeoCoordinate point2A = new GeoCoordinate()
+            {
+                Latitude = 32.00,
+                Longitude = -85.00
+            };
+            GeoCoordinate point2B = new GeoCoordinate()
+            {
+                Latitude = 32.50,
+                Longitude = -85.50
+            };
             // In METERS multiply by .00062137 miles/meter
             return point2A.GetDistanceTo(point2B) * 0.00062137;
         }
     }
-
 }
