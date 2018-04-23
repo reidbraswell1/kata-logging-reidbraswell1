eb38365cce1b4e6ef192bbed3a409d3e65126f24
reid <reidbras@gmail.com>
2018-04-23 10:05:40 -0500

    Adding miles/meter constant

--- LoggingKata/Globals.cs
+++ LoggingKata/Globals.cs
@@ -18,5 +18,6 @@ namespace LoggingKata
         public const char splitChar = ',';
         public const string argumentExceptionLongitude = "Invalid Value for longitude greater than 180.00 or less than -180.00";
         public const string argumentExceptionLatitude = "Invalid Value for latitude greater than 90.00 or less than -90.00";
+        public const double milesPerMeter = 0.00062137;
     }
 }
