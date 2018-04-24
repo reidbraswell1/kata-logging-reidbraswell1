1d9d4d0b1d4cea7b5773deacc2a609b9be481088
reid <reidbras@gmail.com>
2018-04-23 21:38:21 -0500

    Changed to use enum for the log level instead of using integer values. Invalid values can't be set.

--- LoggingKata/TacoLogger.cs
+++ LoggingKata/TacoLogger.cs
@@ -4,17 +4,11 @@ namespace LoggingKata
 {
     public class TacoLogger : ILog
     {
-        public const int fatal = 1;
-        public const int error = 2;
-        public const int warn = 3;
-        public const int info = 4;
-        public const int debug = 5;
-
-        private static int logLevel = fatal;
+        private static LogLevel logLevel = LogLevel.Fatal;
 
         public void LogFatal(string log, Exception exception = null)
         {
-            if (logLevel >= fatal)
+            if (logLevel >= LogLevel.Fatal)
             {
                 Console.WriteLine($"Fatal: {log}, Exception {exception}");
             }
@@ -22,7 +16,7 @@ namespace LoggingKata
 
         public void LogError(string log, Exception exception = null)
         {
-            if (logLevel >= error)
+            if (logLevel >= LogLevel.Error)
             {
                 Console.WriteLine($"Error: {log}, Exception {exception}");
             }
@@ -30,7 +24,7 @@ namespace LoggingKata
 
         public void LogWarning(string log)
         {
-            if (logLevel >= warn)
+            if (logLevel >= LogLevel.Warn)
             {
                 Console.WriteLine($"Warning: {log}");
             }
@@ -38,7 +32,7 @@ namespace LoggingKata
 
         public void LogInfo(string log)
         {
-            if (logLevel >= info)
+            if (logLevel >= LogLevel.Info)
             {
                 Console.WriteLine($"Info: {log}");
             }
@@ -46,44 +40,35 @@ namespace LoggingKata
 
         public void LogDebug(string log)
         {
-            if (logLevel >= debug)
+            if (logLevel >= LogLevel.Debug)
             {
                 Console.WriteLine($"Debug: {log}");
             }
         }
-        public void SetLogLevel(int level)
+        public void SetLogLevel(LogLevel level)
         {
-            var levelText = "";
-            if (level > 0 && level < 6)
-            {
-                logLevel = level;
-                levelText = getLevelText(level);
-                Console.WriteLine($"Log level set to {levelText}");
-            }
-            else
-            {
-                levelText = getLevelText(level);
-                Console.WriteLine($"Log Level Invalid {level} currently set to {levelText}");
-            }
+            var levelText = getLevelText(level);
+            logLevel = level;
+            Console.WriteLine($"Log Level is set to {levelText}");
         }
-        private static string getLevelText(int level)
+        private static string getLevelText(LogLevel level)
         {
             var levelText = "";
             switch (level)
             {
-                case TacoLogger.fatal:
+                case LogLevel.Fatal:
                     levelText = "FATAL";
                     break;
-                case TacoLogger.error:
+                case LogLevel.Error:
                     levelText = "ERROR";
                     break;
-                case TacoLogger.warn:
+                case LogLevel.Warn:
                     levelText = "WARN";
                     break;
-                case TacoLogger.info:
+                case LogLevel.Info:
                     levelText = "INFO";
                     break;
-                case TacoLogger.debug:
+                case LogLevel.Debug:
                     levelText = "DEBUG";
                     break;
             }
