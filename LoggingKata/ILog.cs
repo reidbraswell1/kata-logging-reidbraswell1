4f69e8d5e69f10d1f6ad18d661d892fb0ecd81a8
reid <reidbras@gmail.com>
2018-04-23 21:34:27 -0500

    Changed to use enum for setting the log level.

--- LoggingKata/ILog.cs
+++ LoggingKata/ILog.cs
@@ -8,6 +8,14 @@ namespace LoggingKata
         void LogWarning(string log);
         void LogInfo(string log);
         void LogDebug(string log);
-        void SetLogLevel(int level);
+        void SetLogLevel(LogLevel level);
+    }
+    public enum LogLevel
+    {
+        Fatal,
+        Error,
+        Warn,
+        Info,
+        Debug
     }
 }
